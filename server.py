16ba9348f3187206692cabecce4e501feb9f32c2
shoy160 <shoy160@gmail.com>
2020-07-09 16:45:50 +0800

    exit tornado server

--- server.py
+++ server.py
@@ -11,6 +11,14 @@ import services.image
 import services.k8s
 import services.view
 from services.app import app
+import platform
+import signal
+
+if platform.system() == "Windows":
+    import asyncio
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+is_closing = False
+
 
 define("port", default=8080, help="run on the given port", type=int)
 define("registry-url", default="http://192.168.0.231:5000",
@@ -24,14 +32,30 @@ define("root-account", default='root', help="Root Account", type=str)
 define("root-pwd", default='docker-k8s', help="Root Password", type=str)
 
 
+def signal_handler(signum, frame):
+    global is_closing
+    logging.info('exiting...')
+    is_closing = True
+
+
+def try_exit():
+    global is_closing
+    if is_closing:
+        # clean up here
+        tornado.ioloop.IOLoop.instance().stop()
+        logging.info('exit success')
+
+
 if __name__ == "__main__":
     # 转换命令行
     tornado.options.parse_command_line()
-#     app.preload()
+    signal.signal(signal.SIGINT, signal_handler)
+    # app.preload()
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     # http_server.bind(options.port)
     logger = logging.getLogger('server')
     logger.info("listening on " + str(options.port))
-#     tornado.ioloop.PeriodicCallback(f2s, 2000).start()
+    # tornado.ioloop.PeriodicCallback(f2s, 2000).start()
+    tornado.ioloop.PeriodicCallback(try_exit, 100).start()
     tornado.ioloop.IOLoop.instance().start()
